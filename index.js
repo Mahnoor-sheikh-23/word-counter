c2bf7c8e18ec88b0aac968a8df8ee6e98a3b2520
mahnoor adnan <mahnooradnan479@gmail.com>
2024-04-17 08:48:05 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.magentaBright(`\n\t\t  ~~~~~~~ HEY WELLCOME TO THIS WORD COUNTER ~~~~~~~ \n`));
+let user = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: chalk.rgb(125, 199, 280)("Write paragraph here ...\n")
+    }]);
+let whiteSpace = user.sentence.trim().split(" ");
+console.log(whiteSpace);
+console.log(chalk.yellowBright(`\n\t\tThe total word in this sentence is :  ${whiteSpace.length} \n`));
